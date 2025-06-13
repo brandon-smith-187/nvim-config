7e38d68fc6a98568a62d02869d491fe5d8fc8679
Brandon Smith <bcsmith2@gmail.com>
2025-06-12 20:15:24 -0400

    updated keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -57,7 +57,6 @@ vim.keymap.set("n", "<F2>", ":w<CR>", { desc = "save from insert mode with F2" }
 vim.keymap.set("n", "<F11>", ":AerialToggle<CR>", { desc = "Toggle aerial with F11" })
 vim.keymap.set("n", "<F10>", ":GoPkgOutline<CR>", { desc = "Get Package outline in go file with F10" })
 vim.keymap.set("n", "<F9>", ":GoDoc<CR>", { desc = "Open Go Docs with F9" })
-vim.keymap.set("n", "<F8>", ":AerialToggle<CR>|:NvimTreeToggle<CR>", { desc = "Toggle aerial and tagbar with F8" })
 vim.keymap.set("n", "<F5>", ":TermExec cmd='go run %'<CR>", { desc = "Run current file in go" })
 vim.keymap.set("n", "<F6>", ":TermExec cmd='python %'<CR>", { desc = "Run current file in python" })
 vim.keymap.set("n", "<F7>", ":TermExec cmd='python'<CR>", { desc = "Open python interpreter" })
