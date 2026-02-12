10b593dc89176b3ee240bf42586c6e39c32a5a81
Brandon Smith <brandon.smith@bitsighttech.com>
2026-02-12 11:12:13 -0500

    install script update

--- linuxinstall.sh
+++ linuxinstall.sh
@@ -1,16 +1,20 @@
-#!/usr/bin/zsh
+#!/usr/bin/bash
 
+sudo add-apt-repository ppa:longsleep/golang-backports
+sudo add-apt-repository ppa:neovim-ppa/unstable
 sudo apt update
 sudo apt install git make unzip gcc fonts-noto-color-emoji ripgrep
 
-sudo apt-get install software-properties-common
-sudo add-apt-repository ppa:neovim-ppa/unstable
-sudo add-apt-repository ppa:longsleep/golang-backports
+sudo apt install software-properties-common
+
 curl https://sh.rustup.rs -sSf | sh
 
-sudo apt update
+curl -Ss https://astral.sh | bash
+uv tool install --upgrade pynvim
+
 sudo apt install golang-go
-sudo apt install neovim
+sudo apt install python3-neovim
+sudo apt install python-dev python-pip
 sudo apt install nodejs
 sudo apt install npm
 
@@ -20,3 +24,5 @@ wget -P ~/.local/share/fonts https://github.com/ryanoasis/nerd-fonts/releases/do
 && rm Meslo.zip \
 && fc-cache -fv
 cd ~
+
+sudo apt install neovim
